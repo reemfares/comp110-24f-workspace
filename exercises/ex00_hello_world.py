fd05b81ee5cef3dfbcfd5304ef1a022b00aebd46
Reem Fares <rfares@unc.edu>
2024-08-24 19:20:22 -0400

    Finished Exercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,8 @@
+"""My first exercise in COMP110!"""
+
+__author__ = "730738053"
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+    return "Hello, " + name + "!"
