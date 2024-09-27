714c28205f4469d552ecb3ae58a9cfd8ac21afd4
Reem Fares <rfares@unc.edu>
2024-09-27 19:15:58 -0400

    cq04

--- CQs/cq04/visualize.py
+++ CQs/cq04/visualize.py
@@ -8,5 +8,5 @@ __author__ = 730738053
 x: str = "123"
 y: str = "abc"
 
-print(concat(x, y))  # calling concat from concatenation
-print(get_coords(x, y))  # calling get_coords from coordinates
+concat(x, y)  # calling concat from concatenation
+get_coords(x, y)  # calling get_coords from coordinates
